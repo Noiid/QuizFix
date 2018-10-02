7ead7247a7678f8ab90df4cd66e2a56542a989cf
dionmaulana <mwilliawarma13@gmail.com>
2018-10-02 16:37:49 +0700

    quiz commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { ServersComponent } from './servers/servers.component';
 import { TugasComponent } from './tugas/tugas.component';
 import { Tugas3Component } from './tugas3/tugas3.component';
 import { PegawaiComponent } from './pegawai/pegawai.component';
+import { QuizDionComponent } from './quiz-dion/quiz-dion.component';
+import { QuizHeaderComponent } from './quiz-header/quiz-header.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { PegawaiComponent } from './pegawai/pegawai.component';
     ServersComponent,
     TugasComponent,
     Tugas3Component,
-    PegawaiComponent
+    PegawaiComponent,
+    QuizDionComponent,
+    QuizHeaderComponent
   ],
   imports: [
     BrowserModule,
